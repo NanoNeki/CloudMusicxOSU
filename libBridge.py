28f18924fb9b420466e602824fc1fce1421ee859
NanoNeki <1651307755@qq.com>
2018-08-09 10:03:36 +0800

    FIX:Resolve dependencies between UA and obm_human. FIX:Script raising network errors upon connection failure. UPDATE:Now using interal dload interface to process Playlist fetches. FEAT:Automatically renaming beatmaps according to Bloodcat parameters.

--- libBridge.py
+++ libBridge.py
@@ -4,14 +4,23 @@ import json
 import requests
 import langid
 import difflib
+import os
+# from requests import adapters
 
+# Experimental Code!!!
+# requests.adapters.DEFAULT_RETRIES = 5
+
+# Resources
 debugFlag = 0
 debugObject = ""
 urlBloodCatQuery_Base = "https://bloodcat.com/osu/?mod=json&q="
 urlBloodCatDload_Base = "https://bloodcat.com/osu/s/"
 urlCloudMusicPlaylist_Base = "http://music.163.com/api/playlist/detail?id="
 fileName_Base = ".osz"
-strCookie = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzM3MjY4MTcsImlwIjoiMTgyLjIwMC4xMjkuMTQzIiwidWEiOjMzNjIwMTI5NTd9.i3yHwOSgkxLeh3dVSxkwNKbTVygnA8sYAU1Z7hyMHaM"
+strCookie = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzM3NzUxNzQsImlwIjoiMTgyLjIwMC4xMjkuMTQzIiwidWEiOjMzNjIwMTI5NTd9.9JwsQIDDgcVVOIC4PX4-OEVIN1PVsXScyST5WAGLquY"
+strUA = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
+sessionBloodCat = ""
+
 # Override print() for CMD users
 def print_gbk(str):
     print(str.decode("utf-8").encode("gbk"))
@@ -20,19 +29,29 @@ def print_gbk(str):
 def input_gbk_numbersonly(str):
     return int(raw_input(str.decode("utf-8").encode("gbk")))
 
+#检查并设置User Agent （UA和Cookies是直接相关的，必须同时刷新）
+def getUA(__op = 0):
+    global strUA
+    if strUA == "" or __op == -1:
+        strUA = raw_input("请输入一个UA：".decode("utf-8").encode("gbk"))
+    return strUA
+
 # 检查并设置captcha cookie
-def getCookie(op = 0):
+def getCookie(__op = 0):
     global strCookie
     cookieHeader = "obm_human="
-    if strCookie == "" or op == -1:
+    if strCookie == "" or __op == -1:
         strCookie = raw_input("请输入一个Cookie：".decode("utf-8").encode("gbk"))
+        if strCookie == 'debug':
+            raise RuntimeError
     return cookieHeader + strCookie
 
 # 刷新headers
 def setHeaders():
     cookie = getCookie()
+    ua = getUA()
     __headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36',
+        'user-agent': ua,
         'referer': 'https://bloodcat.com/osu',
         'cookie': cookie}
     return __headers
@@ -50,11 +69,38 @@ def getConverter():
     conv = kakasi.getConverter()
     return conv
 
+# 获取活动连接
+def getSession():
+    global sessionBloodCat
+    if sessionBloodCat == "":
+        refreshSession()
+    return sessionBloodCat
+
+# 刷新连接
+def refreshSession():
+    global sessionBloodCat
+    sessionBloodCat = requests.session()
+    sessionBloodCat.headers.update(setHeaders())
+
 # 通用下载接口 返回-1会强制触发cookie检查
-def dloadFile(url,fileName,headers = {}):
+def dloadFile(url,fileName = '',headers = {}):
     print_gbk ("正在下载：" + url)
-    r = requests.get(url, headers=headers)
+    # FINISHED:复用连接并加入重试
+    # r = requests.get(url,headers=headers)
+    s = getSession()
+    if headers != {}:
+        s = requests.session()
+        s.headers.update(headers)
+    try: # 监测网络错误
+        r = s.get(url)
+    except:
+        print_gbk("正在重试.....")
+        return -1 # -1 for network error
     if r.status_code == 200:
+        if fileName != '':
+            fileName = fileName
+        else :
+            fileName = r.headers['Content-Disposition'].split('"')[1]
         print_gbk("下载完成，正在写入文件：" + fileName)
         file = open(fileName, 'wb')
         for chunk in r.iter_content(100000):
@@ -62,14 +108,17 @@ def dloadFile(url,fileName,headers = {}):
         file.close()
         return 0
     else:
-        # 调试代码
+        # ↓调试代码
         if debugFlag == 1:
             global debugObject
             debugObject = r
+            print(r.status_code)
             return 0
-        # 调试代码
-        print_gbk("下载失败，请检查Cookie！")
+        # ↑调试代码
+        print_gbk("下载失败，请检查参数！")
         getCookie(-1)
+        getUA(-1)
+        refreshSession()
         return -1
 
 # 使用网易云ID搜索血猫，返回谱面ID(list)
@@ -79,7 +128,12 @@ def retrieveSongFromID(__songID,workMode = 0): # Unconverted
     targetSongName = targetSongDetail['name']
     url = urlBloodCatQuery_Base + targetSongName + "&c=b&p=1&s=&m=&g=&l="
     r = requests.get(url)
-    __dictResult = json.loads(r.content)
+    try:
+        __dictResult = json.loads(r.content)
+    except ValueError:
+        print_gbk("网络错误，通常重试一下就可以解决，程序即将退出...")
+        os.system('pause')
+        __dictResult = {} # 我恨死语法检查了
     __matchSongID = []
     conv = getConverter()
     if langid.classify(targetSongName)[0] == 'ja':
@@ -115,7 +169,10 @@ def retrieveSongFromID(__songID,workMode = 0): # Unconverted
                 __matchSongID.append(int(__eachSong['id'].decode('utf-8')))
                 if workMode == 0:
                     break
-    print_gbk("匹配到的谱面ID：" + str(__matchSongID))
+    if __matchSongID != []:
+        print_gbk("匹配到的谱面ID：" + str(__matchSongID))
+    else: print_gbk("没有找到匹配！")
+
     return __matchSongID
 
 # 从网易云歌单获取歌曲名字，返回歌曲名字(unicode,list)
@@ -140,9 +197,20 @@ def retrieveIDFromPlaylist(__playlistID):
     url = urlCloudMusicPlaylist_Base + str(__playlistID)
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'}
     print_gbk("如果歌单曲目较多（超过500首），可能需要较长时间获取，请耐心等待...")
-    print_gbk("出现Connnection Broken错误即为网络不稳定，可以多试几次")
-    r = requests.get(url,headers = headers)
-    __dictResult = json.loads(r.content)
+    print_gbk("出现Connection Broken错误即为网络不稳定，可以多试几次")
+
+    dloadFile(url,'nowWorkingOn.list',headers)
+    f = open('nowWorkingOn.list','r')
+    __content = f.read()
+    f.close()
+
+    try:
+        __dictResult = json.loads(__content)
+    except ValueError:
+        print_gbk("网络错误，通常重试一下就可以解决，程序即将退出...")
+        os.system('pause')
+        __dictResult = {} # 我恨死语法检查了
+
     print_gbk ("歌单名称："),
     print (__dictResult['result']['name'])
     print_gbk ("歌曲数："),
@@ -150,6 +218,7 @@ def retrieveIDFromPlaylist(__playlistID):
     trackIDs = []
     for __eachTrack in __dictResult['result']['tracks']:
         trackIDs.append(__eachTrack['id'])
+    os.remove('nowWorkingOn.list')
     return trackIDs
 
 # 使用 **网易云ID** 下载谱面
@@ -158,13 +227,11 @@ def dloadBeatmapFromID(__songID):
     headers = setHeaders()
     for __eachID in matchSongID:
         url = urlBloodCatDload_Base + str(__eachID)
-        while(dloadFile(url,str(__eachID) + fileName_Base,headers) != 0):
-            headers = setHeaders()
+        while dloadFile(url) == -1:
+            dloadFile(url)
 
 # 从网易云歌单下载谱面
 def dloadBeatmapFromPlaylist(__playlistId):
     trackIDs = retrieveIDFromPlaylist(__playlistId)
     for __eachID in trackIDs:
         dloadBeatmapFromID(__eachID)
-
-# convertedTargetSongName = conv.do(targetSongName)
